58c984bfb13382e5bbbc4746922aa7456ca6f119
WillKirkhope <kirkhopetraining@yahoo.com>
2018-07-20 10:05:17 -0600

    first commit

---
+++ api/studentHomeTown.js
@@ -0,0 +1,26 @@
+module.exports = [{
+  id: 1,
+  firstName: "",
+  lastName: "",
+  homeTown:
+},{
+  id: 1,
+  firstName: "",
+  lastName: "",
+  homeTown:
+},{
+  id: 1,
+  firstName: "",
+  lastName: "",
+  homeTown:
+},{
+  id: 1,
+  firstName: "",
+  lastName: "",
+  homeTown:
+},{
+  id: 1,
+  firstName: "",
+  lastName: "",
+  homeTown:
+}]
